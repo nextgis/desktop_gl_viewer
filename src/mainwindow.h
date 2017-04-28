a95473c74cad262a40ee782ff8ea301216c1e184
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-04-28 04:35:54 +0300

    Add map relative path support. Update headers

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -3,7 +3,7 @@
 *  Purpose: GUI viewer for spatial data.
 *  Author:  Dmitry Baryshnikov, bishop.dev@gmail.com
 *******************************************************************************
-*  Copyright (C) 2016 NextGIS, <info@nextgis.com>
+*  Copyright (C) 2016-2017 NextGIS, <info@nextgis.com>
 *
 *   This program is free software: you can redistribute it and/or modify
 *   it under the terms of the GNU General Public License as published by
